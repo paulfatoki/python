26976b496d0be5eb6c59dc29438403ebcd825c71
paulfatoki <paul.fatoki.itb@gmail.com>
2024-05-11 09:10:40 -0400

    i created python files

---
+++ python1.py
@@ -0,0 +1,11 @@
+i a = 4
+b = 6
+c = a * b
+print(c)
+d = 50
+e = c * d
+print('this is my final result ' + str(e))
+if e > 100: 
+    print('the result is greater than 100')
+else:
+    print('the result is less than 100')
---
+++ python2.py
@@ -0,0 +1,14 @@
+i a = 100
+b = 21
+c = 31
+
+total = a + b + c
+d = 20
+result = total - d
+print(result)
+if result < 50:
+   print('my result is less than 50')
+else:
+    print('my result is greater than 50')
+ 
+
